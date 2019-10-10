b07a3fa7c3f1b20a254fd765081997df767bc96f
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-10 09:27:42 -0400

    add of new strdup

--- my_strdup.c
+++ my_strdup.c
@@ -4,24 +4,21 @@
 ** File description:
 ** one piece
 */
-
+#include <unistd.h>
 #include <stdlib.h>
 
 int my_strlen (char const *str);
-char* my_strcpy (char *dest, char const *src);
+char *my_strcpy (char *dest, char const *src);
 
 char *my_strdup(char const * src)
 {
-    char* str;
-    int longueur = my_strlen(src);
+    char  *str;
     
-    str = malloc(sizeof(int) * (longueur +1));
-    
+    str = malloc(sizeof(char) * (my_strlen(src) +1));
+
     if ( str == NULL) {
         return (NULL);
     }
     str = my_strcpy(str, src);
-    str[longueur - 1] = '\0';
     return (str);
-        
 }
